010676cf4133a3e1a78abb4988fa4ee585c86d5b
gopalkatariya <katariyagopal60@gmail.com>
2022-09-01 09:48:44 +0530

    dir done

--- PWD/Flask/mvc_orm_app/app.py
+++ PWD/Flask/mvc_orm_app/app.py
@@ -1,4 +1,31 @@
+# from base import app
+#
+# if __name__ == '__main__':
+#     app.run()
+
+# import waitress
+
 from base import app
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(threaded=True, debug=True, port=8081)
+    # waitress.serve(app, listen='0.0.0.0:5003')
+
+    # deploy on gunicorn server
+    # gunicorn --bind 0.0.0.0:port_number app:app
+
+    # to kill the port
+    # fuser -n tcp -k port_number
+
+    # python flask orm libs
+    # pip install flask
+    # pip install pymysql
+    # pip install sqlalchemy
+    # pip install flask-sqlalchemy
+    # pip install Flask-Migrate
+
+    # db migrations command
+    # flask db init
+    # flask db migrate -m "migration_one"
+    # flask db upgrade
+    # flask db --help
--- PWD/Flask/mvc_orm_app/base/__init__.py
+++ PWD/Flask/mvc_orm_app/base/__init__.py
@@ -1,8 +1,27 @@
+import warnings
+from datetime import timedelta
+
+import SQLAlchemy as SQLAlchemy
 from flask import Flask
+
+
+warnings.filterwarnings("ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 app = Flask(__name__)
 
+app.secret_key = 'qazwsxedcrfvtgbyhnujmiklop123456'
 
-# @app.route('/')
-# def home():
-#     return 'Hello World!'
+app.config['SQLALCHEMY_ECHO'] = True
+
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'mysql+pymysql://root:root@localhost:3306/flaskdb'
+
+app.config['SQLALCHEMY_MAX_OVERFLOW'] = 0
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from base.com import controller
